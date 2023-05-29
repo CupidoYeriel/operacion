658c1a22b5081809f669705d3723d1eeeffced83
CupidoYeriel <134957524+CupidoYeriel@users.noreply.github.com>
2023-05-29 11:43:02 -0500

    Operacion2

---
+++ Clases/Persona.cs
@@ -0,0 +1,43 @@
+﻿using Ejercicio_1.Funciones;
+using System;
+
+namespace Ejercicio_1.Clases
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Carrera { get; set; }
+        public int Edad { get; set; }
+
+        public void PedirDatos()
+        {
+            Console.WriteLine("Ingresa tu nombre");
+            this.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Apellido");
+            this.Apellido = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Carrera");
+            this.Carrera = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Edad");
+            this.Edad = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            if (Edad >= 18)
+            {
+                Menu menu = new Menu();
+
+                menu.Opciones();
+            }
+            else
+            {
+                Console.WriteLine("No puedes hacer las operaciones por no ser legal");
+            }
+        }
+
+
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Ejercicio_1.Clases;
+using System;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Persona persona = new Persona();
+
+            persona.PedirDatos();
+        }
+    }
+}
